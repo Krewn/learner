70b484b3ce60bfada762c2fe6c07052511e8d6a8
Kevin Nelson <kpie314@gmail.com>
2016-03-30 02:19:16 -0700

    est.

---
+++ FieldPredictor.py
@@ -0,0 +1,103 @@
+###########################################################    ____                   ______ _      _     _
+##   Author : Kevin Nelson                             ####  / ____|                 |  ____(_)    | |   | |
+#		kpie314@gmail.com   ArtisticLicense             ### | |  __  ___ _ __   ___  | |__   _  ___| | __| |___
+##      Creating predictive fields from given data     #### | | |_ |/ _ \ '_ \ / _ \ |  __| | |/ _ \ |/ _` / __|
+########################################################### | |__| |  __/ | | |  __/ | |    | |  __/ | (_| \__ \
+###########################################################  \_____|\___|_| |_|\___| |_|    |_|\___|_|\__,_|___/
+
+def Min(a):
+    m = None
+    for k in a:
+        if (k != None):
+            m = k if (k < m or m is None)else m
+    return m
+
+
+def Max(a):
+    m = None
+    for k in a:
+        if (k != None):
+            m = k if (k > m or m is None)else m
+    return m
+
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+
+def colMin(data, col):
+    return (Min([data[k][col] for k in range(0, len(data))]))
+
+
+def colMax(data, col):
+    return (Max([data[k][col] for k in range(0, len(data))]))
+
+
+def EucAB(a, b):
+    dist = 0.
+    if (len(a) != len(b)): raise ValueError('Vectors are not of similar dimensionality.')
+    for k in range(0, len(a)):
+        if (a[k] != None and b[k] != None):
+            dist += (a[k] - b[k]) ** 2
+    dist = dist ** 0.5
+    return (dist)
+
+
+def EucABdims(a, b):
+    dist = 0.
+    d = 0
+    if (len(a) != len(b)): raise ValueError('Vectors are not of similar dimensionality.')
+    for k in range(0, len(a)):
+        if (a[k] != None and b[k] != None):
+            dist += (a[k] - b[k]) ** 2
+            d += 1
+    dist = dist ** 0.5
+    return ((dist, d))
+
+
+def fetchpPrediction(data, row, col):  # Samples and assays as rows and columns
+    p = 0.  # prediction = 0
+    bot = 0.
+    m = colMax(data, col) - colMin(data, col)
+    if(m == 0):
+        return(colMax(data, col))
+    for k in range(0, len(data)):
+        temp = EucABdims(data[k], data[row])
+        euc = temp[0]  # the euclidean distance between two samples
+        dims = temp[1]  # the number of genes in the union of two samples data
+        if (k != row and dims != 0 and data[k][col] != None):  # does not use samples with 0** common genes to make predictions.
+            p += data[k][col] / ((euc+0.000001) ** 2)  # Adds to a weighted sum
+            bot += m / ((euc+0.000001) ** 2)  # Adds to a weighted averager    ## poorMan's limit
+    return ( colMin(data, col)+m*(p / bot) )
+
+
+a = [[0., 0., 1.],
+     [0., None, 1.],
+     [0., 1, 0.]]
+
+print(str(fetchpPrediction(a, 1, 1))) #Example filling in a blank value
+
+def cook(data):
+    r = [ k[:] for k in data ] #makes a detached copy from
+    for k in range(0,len(r)):
+        for k2 in range(0,len(r[k])):
+            if(r[k][k2]==None):
+                r[k][k2]=fetchpPrediction(a, k, k2)
+    return(r)
+
+def pr(data):
+    s = ""
+    for k in data:
+        for k2 in k:
+            s += str(round(k2,5) if k2!= None else "Nan")+"\t"*3
+        s += "\n"
+    return(s)
+
+print("In:")
+print(pr(a))
+print("Out:")
+print(pr(cook(a))) # example batch run for a table
---
+++ make.py
@@ -0,0 +1,93 @@
+import FieldPredictor as fp
+from operator import itemgetter
+import time
+def spacePrt(s,l):
+    if(type(s) == float):
+        s = round(s,3)
+    return(str(s)+(" "*(l-len(str(s))) if len(str(s))<= l else "#"))
+
+def colPrt(s,l):
+    if(type(s) == float):
+        s = round(s,3)
+    CSI="\x1B["
+    return(CSI+"31;40m" + str(s) + CSI + "0m"+(" "*(l-len(str(s))) if len(str(s))<= l else "#"))
+
+class smarty():
+    things = [] # row names
+    attrs = [] # col names
+    data = []
+    predTable = []
+    def learn(self,s):
+        print(s)
+        s = s.lower()
+        s = s.split(" ")
+        rn = s[0]
+        cn = s[len(s)-1]
+        neg = False
+        neg = True if "not" in s else False
+        if len(self.data) == 0:
+            self.data.append([None])
+            self.attrs.append(cn)
+            self.things.append(rn)
+        else:
+            if (rn in self.things) == False:
+                self.things.append(rn)
+                self.data.append([None]*len(self.data[0]))
+            if (cn in self.attrs) == False:
+                self.attrs.append(cn)
+                for k in self.data:
+                    k.append(None)
+        self.data[self.things.index(rn)][self.attrs.index(cn)] = 0 if neg else 1
+        print(self.data)
+
+    def think(self):
+        predictions = []
+        for n,k in enumerate(self.data):
+            for n2,k2 in enumerate(k):
+                if(k2 == None):
+                    predictions.append((self.things[n],self.attrs[n2],fp.fetchpPrediction(self.data, n, n2)))
+        thirdCol = map(itemgetter(2), predictions)
+        Pos = thirdCol.index(max(thirdCol))
+        Neg = thirdCol.index(max(thirdCol))
+        print(predictions[Pos])
+        print(predictions[Neg])
+    def prt(self):
+        out = ""
+        out+=spacePrt("",15)
+        for k in self.attrs:
+            out+=spacePrt(k,15)
+        for n,k in enumerate(self.data):
+            out+="\n"+spacePrt(self.things[n],15)
+            for k2 in k:
+                out+=spacePrt(str(k2),15)
+        print("\n\n"+out+"\n\n")
+    def predPrt(self):
+        out = ""
+        out+=spacePrt("",15)
+        for k in self.attrs:
+            out+=spacePrt(k,15)
+        for n,k in enumerate(self.data):
+            out+="\n"+spacePrt(self.things[n],15)
+            for n2,k2 in enumerate(k):
+                if(k2 == None):
+                    out+=colPrt(fp.fetchpPrediction(self.data,n,n2),15)
+                else:
+                    out+=spacePrt(str(k2),15)
+        print("\n\n"+out+"\n\n")
+
+a = smarty()
+a.learn("dog bark")
+a.learn("dog is warmBlooded")
+a.learn("fish is not warmBlooded")
+a.learn("fish has scales")
+a.learn("lizard not warmBlooded")
+a.learn("cat is warmBlooded")
+a.learn("cat does not have scales")
+a.learn("cat does not bark")
+
+#a.learn("Sas likes IWW")
+#a.learn("mom likes IWW")
+#a.learn("mom likes dearFranky")
+
+a.predPrt()
+a.think()
